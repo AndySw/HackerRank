6865e699f0a66c38b3155aa74787997cb086dc25
Andy Sweetman <adsweetman@gmail.com>
2017-06-25 13:36:40 +0100

    Initial commit for Making Anagrams

---
+++ HackerRank.MakingAnagrams/Solution.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.MakingAnagrams
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+        }
+    }
+}
---
+++ HackerRank.UnitTests/MakingAnagramsTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HackerRank.UnitTests
+{
+    [TestClass]
+    public class MakingAnagramsTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
